fb73b20420ed226be8aceff8de194f6a0d1cec82
Paulo ELFF <kiritoscorpionkun@gmail.com>
2023-09-08 21:34:24 -0300

    configuracao inicial

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './style.css'
+
+function App() {
+  return (
+    <div>
+    </div>
+  )
+}
+
+export default App
---
+++ src/Hooks/useFetch.tsx
@@ -0,0 +1,43 @@
+import React, { useEffect, useRef, useState } from "react"
+
+function useFetch<T>(url: RequestInfo | URL, options?: RequestInit) {
+  const [data, setData] = useState<T | null>(null)
+  const [loading, setLoading] = useState(false)
+  const [error, setError] = useState<string | null>(null)
+
+  const optionsRef = useRef(options)
+  optionsRef.current = options
+
+  useEffect(() => {
+    const controller = new AbortController()
+    const {signal} = controller
+
+    const fetchData = async () => {
+      setLoading(false)
+      setData(null)
+
+      try {
+        const response = await fetch(url, {
+          signal,
+          ...optionsRef.current,
+        })
+        if (!response.ok) throw new Error(`Error: ${response.status}`)
+        const json = await response.json() as T
+        if (!signal.aborted) setData(json)
+
+      } catch (error) {
+        if (!signal.aborted && error instanceof Error) setError(error.message)
+
+      } finally {
+        if (!signal.aborted) setLoading(false)
+      }
+    }
+    fetchData()
+
+    return () => {
+      controller.abort()
+    }
+  }, [url])
+
+  return { data, loading, error }
+}
